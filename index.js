a4d2626daa7279bf3d0ce9151f31d48eb7483d11
Eric Waters <ewaters@shutterstock.com>
2016-03-20 22:33:40 -0400

    Whitespace and cleanup

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 // @match        https://quizlet.com/*/*
 // @grant        none
 // @require      https://cdnjs.cloudflare.com/ajax/libs/async/1.5.2/async.min.js
-// @require      https://raw.githubusercontent.com/zhuker/lamejs/master/lame.min.js
 // ==/UserScript==
 /* jshint -W097 */
 /* global AudioContext, setPage, async, $, lamejs, QModal */
@@ -24,20 +23,19 @@ function installButton() {
 	}
 	var modal = installModal();
 
-    var popout = $('.SetTools-tool.poppable .popout');
-    if (popout.length === 0) {
-        return;
-    }
-    var btn = $('<a class="SetTools-moreTool SetTools-moreTool--audio audio-tool"><span class="glyph icon audio-icon">&#xE096;</span><span class="label">Get Audio</span></a>');
-    popout.append(btn);
-    btn.on("click", function() {
+	var popout = $('.SetTools-tool.poppable .popout');
+	if (popout.length === 0) {
+		return;
+	}
+	var btn = $('<a class="SetTools-moreTool SetTools-moreTool--audio audio-tool"><span class="glyph icon audio-icon">&#xE096;</span><span class="label">Get Audio</span></a>');
+	popout.append(btn);
+	btn.on("click", function() {
 		QModal.open(modal, {
 			on: { open: modalOpened },
 			includeClose: true,
 		});
 	});
-    console.log("Button installed");
-
+	console.log("Button installed");
 }
 
 function installModal() {
@@ -58,13 +56,13 @@ function modalOpened() {
 	var form = $('<form class="qmodal-form"/>');
 	form.append("<label class='field'>" +
 			"<input type='radio' name='terms' value='all' id='terms_all'/> " + 
-			"<label for='terms_all'>All " + setPage.terms.length + " terms</label>" +
+			"<label for='terms_all'>All " + getAllTerms().length + " terms</label>" +
 			"</label>");
 	form.append("<label class='field'>" +
 			"<input type='radio' name='terms' value='star' id='terms_star'/> " +
 			"<label for='terms_star'>Only <span class='glyph icon star-icon'>&#xE041;</span> terms (" + getStarredTerms().length + " items)</label>" +
 			"</label>");
-	var btn = $("<button>Build</button>");
+	var btn = $("<button class='.ReviewFlipControls-button'>Build</button>");
 	btn.on("click", function (e) {
 		generateAudio();
 		e.preventDefault();
@@ -81,9 +79,29 @@ function modalProgress(msg) {
 	section.append("<p>" + msg + "</p>");
 }
 
+function getAllTerms() {
+	if (setPage !== undefined) {
+		return setPage.terms;
+	} else if (Cards !== undefined && Cards.data !== undefined) {
+		return Cards.data.terms;
+	}
+}
+
+function getSetTitle() {
+	var setTitle = $('section.SetHeader .SetTitle-title').text();
+	if (!setTitle) {
+		if (setPage !== undefined) {
+			setTitle = "Set " + window.setPage.setId;
+		} else if (Cards !== undefined && Cards.data !== undefined) {
+			setTitle = "Set " + Cards.data.setId;
+		}
+	}
+	return setTitle;
+}
+
 function getStarredTerms() {
 	var byId = {};
-	$.each(setPage.terms, function(idx, val) {
+	$.each(getAllTerms(), function(idx, val) {
 		byId[val.id] = val;
 	});
 
@@ -96,13 +114,10 @@ function getStarredTerms() {
 }
 
 function generateAudio() {
-    console.time("generateAudio");
-    console.time("fetchTermAudio");
-
 	var terms = [];
 	var subsetTerms = $('.GetAudioModal input[name=terms]:checked').val();
 	if (subsetTerms === "all") {
-		terms = setPage.terms;
+		terms = getAllTerms();
 	} else {
 		terms = getStarredTerms();
 	}
@@ -113,103 +128,97 @@ function generateAudio() {
 	});
 
 	var i = 0;
-    async.mapLimit(terms, 5, 
-		function(term, cb) {
-			i++;
-			progressCallback(i, terms.length);
-			return fetchTermAudio(term, cb);
-		}, fetchComplete);
+	async.mapLimit(terms, 5, 
+			function(term, cb) {
+				i++;
+				progressCallback(i, terms.length);
+				return fetchTermAudio(term, cb);
+			}, fetchComplete);
 }
 
 function fetchTermAudio(term, cb) {
-    term.audio = {};
-    var forEach = function (type, cb) {
-        fetchAudio(term.getAudioUrl(type), function (err, data) {
-            if (err) {
-                return cb(err);
-            }
-            term.audio[type] = data;
-            return cb();
-        });
-    };
-    async.each(["word", "definition"], forEach, function(err) { cb(err, term); });
+	term.audio = {};
+	var forEach = function (type, cb) {
+		fetchAudio(term.getAudioUrl(type), function (err, data) {
+			if (err) {
+				return cb(err);
+			}
+			term.audio[type] = data;
+			return cb();
+		});
+	};
+	async.each(["word", "definition"], forEach, function(err) { cb(err, term); });
 }
 
 function fetchAudio (url, cb) {
-    var request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        context.decodeAudioData(request.response, function (buffer) {
-            cb(null, {
+	var request = new XMLHttpRequest();
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+	request.onload = function () {
+		context.decodeAudioData(request.response, function (buffer) {
+			cb(null, {
 				buffer: buffer,
 				int16: audioBufferToInt16(buffer),
 			});
-        }, function () {
-            cb("Audio " + url + " decodeAudioData failed");
-        });
-    };
-    request.send();
+		}, function () {
+			cb("Audio " + url + " decodeAudioData failed");
+		});
+	};
+	request.send();
 }
 
 function fetchComplete(err, terms) {
-    console.timeEnd("fetchTermAudio");
-    if (err) {
-        console.error(err);
-        return;
-    }
-    var sampleRate = terms[0].audio.word.buffer.sampleRate;
+	if (err) {
+		console.error(err);
+		return;
+	}
+	var sampleRate = terms[0].audio.word.buffer.sampleRate;
 
-    var plan = [];
-    $.each(terms, function(idx, term) {
-        plan.push({ audio: term.audio.definition });
-        plan.push({ silence: 1.0 });
-        
-        plan.push({ audio: term.audio.word });
-        plan.push({ silence: 1.5 });
-        plan.push({ audio: term.audio.word });
-        plan.push({ silence: 2.0 });
-    });
+	var plan = [];
+	$.each(terms, function(idx, term) {
+		plan.push({ audio: term.audio.definition });
+		plan.push({ silence: 1.5 });
 
-    var totalLength = 0;
-    $.each(plan, function(idx, item) {
-        if (item.silence !== undefined) {
-            totalLength += Math.floor(item.silence * sampleRate);
-        }
-        if (item.audio !== undefined) {
-            plan[idx].offset = totalLength;
-            totalLength += item.audio.buffer.length;
-        }
-    });
+		plan.push({ audio: term.audio.word });
+		plan.push({ silence: 1.5 });
+		plan.push({ audio: term.audio.word });
+		plan.push({ silence: 2.0 });
+	});
+
+	var totalLength = 0;
+	$.each(plan, function(idx, item) {
+		if (item.silence !== undefined) {
+			totalLength += Math.floor(item.silence * sampleRate);
+		}
+		if (item.audio !== undefined) {
+			plan[idx].offset = totalLength;
+			totalLength += item.audio.buffer.length;
+		}
+	});
 
 	// Use the plan to generate two items: AudioBuffer and Int16Array.
-    var buf = context.createBuffer(1, totalLength, sampleRate);
-    var channel = buf.getChannelData(0);
+	var buf = context.createBuffer(1, totalLength, sampleRate);
+	var channel = buf.getChannelData(0);
 	var samples = new Int16Array(totalLength);
-    $.each(plan, function(idx, item) {
-        if (item.audio === undefined) {
-            return;
-        }
-        channel.set(item.audio.buffer.getChannelData(0), item.offset);
+	$.each(plan, function(idx, item) {
+		if (item.audio === undefined) {
+			return;
+		}
+		channel.set(item.audio.buffer.getChannelData(0), item.offset);
 		samples.set(item.audio.int16, item.offset);
-    });
-    console.timeEnd("generateAudio");
+	});
 
-	var setTitle = $('section.SetHeader .SetTitle-title').text();
-	if (!setTitle) {
-		setTitle = "Set " + window.setPage.setId;
-	}
+	var setTitle = getSetTitle();
 
-    window.playBuf = function() {
-        playBuffer(buf);
-    };
-    window.downloadWAV = function(cb) {
-        var wav = audioBufferToWav(buf, { int16samples: samples });
+	window.playBuf = function() {
+		playBuffer(buf);
+	};
+	window.downloadWAV = function(cb) {
+		var wav = audioBufferToWav(buf, { int16samples: samples });
 		downloadBinaryFile(setTitle + ".wav", "audio/wav", [ new DataView(wav) ]);
 		QModal.close();
-    };
+	};
 	window.downloadMP3 = function(cb) {
-        console.time("downloadMP3");
 		var progressCallback = progressEvery(500, function(percent, secsRemain) {
 			if (secsRemain === undefined) {
 				secsRemain = "Unknown";
@@ -225,7 +234,6 @@ function fetchComplete(err, terms) {
 		});
 		var completeCallback = function(data) {
 			downloadBinaryFile(setTitle + ".mp3", "audio/mp3", data);
-			console.timeEnd("downloadMP3");
 			QModal.close();
 		};
 		encodeMP3Worker(samples, sampleRate, progressCallback, completeCallback);
@@ -239,7 +247,7 @@ function fetchComplete(err, terms) {
 	section.empty();
 	var form = $('<form class="qmodal-form"/>');
 	$.each(items, function(idx, item) {
-		var btn = $("<button>" + item.label + "</button>");
+		var btn = $("<button class='.ReviewFlipControls-button'>" + item.label + "</button>");
 		btn.on("click", function (e) {
 			item.action(modalProgress);
 			e.preventDefault();
@@ -271,30 +279,9 @@ function progressEvery (interval, cb) {
 	};
 }
 
-function encodeMP3 (samples, sampleRate, progressCallback) {
-	var lib = new lamejs(),
-		enc = new lib.Mp3Encoder(1, sampleRate, 128),
-		blockSize = 1152,
-		data = [],
-		len = samples.length,
-		i, buf;
-	for (i = 0; i < len; i += blockSize) {
-		progressCallback(i, len);
-		buf = enc.encodeBuffer(samples.subarray(i, i + blockSize));
-		if (buf.length > 0) {
-			data.push(buf);
-		}
-	}
-	buf = enc.flush();
-	if (buf.length > 0) {
-		data.push(buf);
-	}
-	return data;
-}
-
 function fetchLameLib(cb) {
-    var req = new XMLHttpRequest();
-    req.open('GET', 'https://raw.githubusercontent.com/zhuker/lamejs/master/lame.min.js');
+	var req = new XMLHttpRequest();
+	req.open('GET', 'https://raw.githubusercontent.com/zhuker/lamejs/master/lame.min.js');
 	req.addEventListener("load", function() {
 		cb(null, this.responseText);
 	});
@@ -309,28 +296,28 @@ function encodeMP3Worker (samples, sampleRate, progressCallback, completeCallbac
 		}
 
 		js += `
-		self.addEventListener("message", function(e) {
-			var lib = new lamejs(),
-				enc = new lib.Mp3Encoder(1, e.data.sampleRate, 128),
-				blockSize = 1152,
-				data = [],
-				samples = e.data.samples,
-				len = samples.length,
-				i, buf;
-			for (i = 0; i < len; i += blockSize) {
-				self.postMessage({ progress: [i, len] });
-				buf = enc.encodeBuffer(samples.subarray(i, i + blockSize));
+			self.addEventListener("message", function(e) {
+				var lib = new lamejs(),
+					enc = new lib.Mp3Encoder(1, e.data.sampleRate, 128),
+					blockSize = 1152,
+					data = [],
+					samples = e.data.samples,
+					len = samples.length,
+					i, buf;
+				for (i = 0; i < len; i += blockSize) {
+					self.postMessage({ progress: [i, len] });
+					buf = enc.encodeBuffer(samples.subarray(i, i + blockSize));
+					if (buf.length > 0) {
+						data.push(buf);
+					}
+				}
+				buf = enc.flush();
 				if (buf.length > 0) {
 					data.push(buf);
 				}
-			}
-			buf = enc.flush();
-			if (buf.length > 0) {
-				data.push(buf);
-			}
-			self.postMessage({ complete: data });
-			self.close();
-		}, false);
+				self.postMessage({ complete: data });
+				self.close();
+			}, false);
 		`;
 
 		var msg = {
@@ -366,136 +353,124 @@ function downloadBinaryFile (filename, type, data) {
 }
 
 function playBuffer (buf) {
-    var source = context.createBufferSource();
-    source.buffer = buf;
-    source.connect(context.destination);
-    source.start(0);
+	var source = context.createBufferSource();
+	source.buffer = buf;
+	source.connect(context.destination);
+	source.start(0);
 }
 
-function appendBuffer (buffer1, buffer2) {
-    var numberOfChannels = Math.min(buffer1.numberOfChannels, buffer2.numberOfChannels);
-    var tmp = context.createBuffer(numberOfChannels, (buffer1.length + buffer2.length), buffer1.sampleRate);
-    for (var i=0; i<numberOfChannels; i++) {
-        var channel = tmp.getChannelData(i);
-        channel.set(buffer1.getChannelData(i), 0);
-        channel.set(buffer2.getChannelData(i), buffer1.length);
-    }
-    return tmp;
-}
-
-/* https://github.com/Jam3/audiobuffer-to-wav/blob/master/index.js */
 function audioBufferToInt16 (buffer) {
-    var float32Arr = buffer.getChannelData(0),
+	var float32Arr = buffer.getChannelData(0),
 		len = float32Arr.length,
 		result = new Int16Array(len),
 		s, i;
-    for (i = 0; i < len; i++) {
-        s = Math.max(-1, Math.min(1, float32Arr[i]));
-        result[i] = s < 0 ? s * 0x8000 : s * 0x7FFF;
-    }
+	for (i = 0; i < len; i++) {
+		s = Math.max(-1, Math.min(1, float32Arr[i]));
+		result[i] = s < 0 ? s * 0x8000 : s * 0x7FFF;
+	}
 	return result;
 }
 
+/* https://github.com/Jam3/audiobuffer-to-wav/blob/master/index.js */
 function audioBufferToWav (buffer, opt) {
-    opt = opt || {};
+	opt = opt || {};
 
-    var numChannels = buffer.numberOfChannels;
-    var sampleRate = buffer.sampleRate;
-    var format = opt.float32 ? 3 : 1;
-    var bitDepth = format === 3 ? 32 : 16;
+	var numChannels = buffer.numberOfChannels;
+	var sampleRate = buffer.sampleRate;
+	var format = opt.float32 ? 3 : 1;
+	var bitDepth = format === 3 ? 32 : 16;
 
-    var samples;
+	var samples;
 	if (opt.int16samples) {
 		samples = opt.int16samples;
 	} else if (numChannels === 2) {
-        samples = interleave(buffer.getChannelData(0), buffer.getChannelData(1));
-    } else {
-        samples = buffer.getChannelData(0);
-    }
+		samples = interleave(buffer.getChannelData(0), buffer.getChannelData(1));
+	} else {
+		samples = buffer.getChannelData(0);
+	}
 
-    var wav = startWAV(samples, format, sampleRate, numChannels, bitDepth);
+	var wav = startWAV(samples, format, sampleRate, numChannels, bitDepth);
 	var view;
 	if (opt.int16samples) {
 		view = new Int16Array(wav);
 		view.set(opt.int16samples, 22);
 	} else if (format === 1) { // Raw PCM
 		view = new DataView(wav);
-        floatTo16BitPCM(view, 44, samples);
-    } else {
+		floatTo16BitPCM(view, 44, samples);
+	} else {
 		view = new DataView(wav);
-        writeFloat32(view, 44, samples);
-    }
+		writeFloat32(view, 44, samples);
+	}
 	return wav;
 }
 
 function startWAV (samples, format, sampleRate, numChannels, bitDepth) {
-    var bytesPerSample = bitDepth / 8;
-    var blockAlign = numChannels * bytesPerSample;
+	var bytesPerSample = bitDepth / 8;
+	var blockAlign = numChannels * bytesPerSample;
 
-    var buffer = new ArrayBuffer(44 + samples.length * bytesPerSample);
-    var view = new DataView(buffer);
+	var buffer = new ArrayBuffer(44 + samples.length * bytesPerSample);
+	var view = new DataView(buffer);
 
-    /* RIFF identifier */
-    writeString(view, 0, 'RIFF');
-    /* RIFF chunk length */
-    view.setUint32(4, 36 + samples.length * bytesPerSample, true);
-    /* RIFF type */
-    writeString(view, 8, 'WAVE');
-    /* format chunk identifier */
-    writeString(view, 12, 'fmt ');
-    /* format chunk length */
-    view.setUint32(16, 16, true);
-    /* sample format (raw) */
-    view.setUint16(20, format, true);
-    /* channel count */
-    view.setUint16(22, numChannels, true);
-    /* sample rate */
-    view.setUint32(24, sampleRate, true);
-    /* byte rate (sample rate * block align) */
-    view.setUint32(28, sampleRate * blockAlign, true);
-    /* block align (channel count * bytes per sample) */
-    view.setUint16(32, blockAlign, true);
-    /* bits per sample */
-    view.setUint16(34, bitDepth, true);
-    /* data chunk identifier */
-    writeString(view, 36, 'data');
-    /* data chunk length */
-    view.setUint32(40, samples.length * bytesPerSample, true);
+	/* RIFF identifier */
+	writeString(view, 0, 'RIFF');
+	/* RIFF chunk length */
+	view.setUint32(4, 36 + samples.length * bytesPerSample, true);
+	/* RIFF type */
+	writeString(view, 8, 'WAVE');
+	/* format chunk identifier */
+	writeString(view, 12, 'fmt ');
+	/* format chunk length */
+	view.setUint32(16, 16, true);
+	/* sample format (raw) */
+	view.setUint16(20, format, true);
+	/* channel count */
+	view.setUint16(22, numChannels, true);
+	/* sample rate */
+	view.setUint32(24, sampleRate, true);
+	/* byte rate (sample rate * block align) */
+	view.setUint32(28, sampleRate * blockAlign, true);
+	/* block align (channel count * bytes per sample) */
+	view.setUint16(32, blockAlign, true);
+	/* bits per sample */
+	view.setUint16(34, bitDepth, true);
+	/* data chunk identifier */
+	writeString(view, 36, 'data');
+	/* data chunk length */
+	view.setUint32(40, samples.length * bytesPerSample, true);
 
-    return buffer;
+	return buffer;
 }
 
 function interleave (inputL, inputR) {
-    var length = inputL.length + inputR.length;
-    var result = new Float32Array(length);
+	var length = inputL.length + inputR.length;
+	var result = new Float32Array(length);
 
-    var index = 0;
-    var inputIndex = 0;
+	var index = 0;
+	var inputIndex = 0;
 
-    while (index < length) {
-        result[index++] = inputL[inputIndex];
-        result[index++] = inputR[inputIndex];
-        inputIndex++;
-    }
-    return result;
+	while (index < length) {
+		result[index++] = inputL[inputIndex];
+		result[index++] = inputR[inputIndex];
+		inputIndex++;
+	}
+	return result;
 }
 
 function writeFloat32 (output, offset, input) {
-    for (var i = 0; i < input.length; i++, offset += 4) {
-        output.setFloat32(offset, input[i], true);
-    }
+	for (var i = 0; i < input.length; i++, offset += 4) {
+		output.setFloat32(offset, input[i], true);
+	}
 }
 
 function floatTo16BitPCM (output, offset, input) {
-    for (var i = 0; i < input.length; i++, offset += 2) {
-        var s = Math.max(-1, Math.min(1, input[i]));
-        output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
-    }
+	for (var i = 0; i < input.length; i++, offset += 2) {
+		var s = Math.max(-1, Math.min(1, input[i]));
+		output.setInt16(offset, s < 0 ? s * 0x8000 : s * 0x7FFF, true);
+	}
 }
 
 function writeString (view, offset, string) {
-    for (var i = 0; i < string.length; i++) {
-        view.setUint8(offset + i, string.charCodeAt(i));
-    }
+	for (var i = 0; i < string.length; i++) {
+		view.setUint8(offset + i, string.charCodeAt(i));
+	}
 }
-
